575b2370c71ad0551dbcf9c93dd973c6ec5cfdba
Enzo Gabriel <eggqsaraiva@gmail.com>
2021-09-09 21:35:46 -0300

    (#3) Criando componente Header

---
+++ app/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <h2>Curumim</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
