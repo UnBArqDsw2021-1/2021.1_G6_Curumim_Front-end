0d515de9ef2576de6d63eecf117d5a6c96ceaa26
Nilo Mendonça <nilojrmendonca@gmail.com>
2021-09-20 08:16:14 -0300

    Adição do HTML das páginas listar turmas e detalhes do evento

--- app/src/routes/index.jsx
+++ app/src/routes/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import ClassList from '../pages/ClassList';
+import EventDetail from '../pages/EventDetail';
 
 class Routes extends Component {
   render() {
@@ -8,6 +10,8 @@ class Routes extends Component {
       <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/listar-turmas" component={ClassList} />
+        <Route exact path="/detalhe-evento" component={EventDetail} />
       </Switch>
     </BrowserRouter>
     );
