b1bb61a55cc7a4755169b9b484120f707c14e685
Joao Pedro Moura <joaopedrohm17@gmail.com>
2021-09-28 19:24:18 -0300

    Adicionando css do "ActivityDetail"

--- app/src/pages/ActivityDetail/index.jsx
+++ app/src/pages/ActivityDetail/index.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 
 export default function ActivityDetail(props) {
     let state = {
-        eventName: "Festa Junina",
+        activityName: "Festa Junina",
         date: "05/08",
         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
     };
@@ -11,11 +11,11 @@ export default function ActivityDetail(props) {
     return (
         <Fragment>
             <div> NavBar </div>
-            <h2>{ props.match.params.value }</h2>
-            <h2>{ state.eventName }</h2>
-            <p>Data { state.date }</p>
-            <button>Editar</button>
-            <p>{ state.description }</p>
+            <header className="App-header-detail">
+                <h2 className="activityName"> { state.activityName }</h2>
+                <p className="data">Data { state.date }</p>
+                <p className="text">{ state.description }</p>
+            </header>
         </Fragment>
     );
 }
