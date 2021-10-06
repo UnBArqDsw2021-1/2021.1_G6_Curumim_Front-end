aea43c41db95bd10cd3b6bd3af9b3400ecb6e5df
enzoggqs <eggqsaraiva@gmail.com>
2021-10-05 21:13:50 -0300

    Adicionando icones na header

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -5,7 +5,10 @@ class Header extends Component {
     render() {
         return (
             <div className="Header">
+                <img src="../../assets/images/left-arrow-white.png" alt="" />
                 <h2>Curumim</h2>
+                <div className="sidebar">
+                </div>
             </div>
         );
     }
