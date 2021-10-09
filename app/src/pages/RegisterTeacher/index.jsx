7cf731e87f99b1c2f8d6a837482dd5ce65c91bca
enzoggqs <eggqsaraiva@gmail.com>
2021-10-09 17:12:11 -0300

    Implementando funcionalidades da tela de registrar professor

--- app/src/pages/RegisterTeacher/index.jsx
+++ app/src/pages/RegisterTeacher/index.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 
 import './styles.css';
 
 function RegisterTeacher() {
+  const [name, setName] = useState('');
+  const [cpfValue, setCPF] = useState('');
+  const [address, setAdress] = useState('');
+  const [cep, setCEP] = useState('');
+  const [cellphone, setCellphone] = useState('');
+  const [email, setEmail] = useState('');
+
+  const addedTeacher = {
+    name: name,
+    cpf: cpfValue,
+    address: address,
+    cep: cep,
+    cellphone: cellphone,
+    email: email
+  }
+
   return(
     <section className="adm-register">
       <Header />
@@ -13,32 +29,32 @@ function RegisterTeacher() {
         <form className="adm-register-form">
           <div className="input-register-teacher">
             <h4>Nome:</h4>
-            <input type="text" placeholder="Nome do professor"/>
+            <input type="text" placeholder="Nome do professor" onChange={(e) => setName(e.currentTarget.value)}/>
           </div>
           <div className="input-register-teacher">
             <h4>CPF:</h4>
-            <input type="number" placeholder="CPF do professor"/>
+            <input type="number" placeholder="CPF do professor" onChange={(e) => setCPF(e.currentTarget.value)}/>
           </div>
           <div className="input-register-teacher">
             <h4>Endereço:</h4>
-            <input type="text" placeholder="Endereço do professor"/>
+            <input type="text" placeholder="Endereço do professor" onChange={(e) => setAdress(e.currentTarget.value)}/>
           </div>
           <div className="input-register-teacher">
             <h4>CEP:</h4>
-            <input type="number" placeholder="CEP do professor"/>
+            <input type="number" placeholder="CEP do professor" onChange={(e) => setCEP(e.currentTarget.value)}/>
           </div>
           <div className="input-register-teacher">
             <h4>Celular:</h4>
-            <input type="number" placeholder="Celular do professor"/>
+            <input type="number" placeholder="Celular do professor" onChange={(e) => setCellphone(e.currentTarget.value)}/>
           </div>
           <div className="input-register-teacher">
             <h4>E-mail:</h4>
-            <input type="email" placeholder="E-mail do professor"/>
+            <input type="email" placeholder="E-mail do professor" onChange={(e) => setEmail(e.currentTarget.value)}/>
           </div>
         </form>
         <div className="adm-register-buttons">
             <button className="adm-cancel-register">Cancelar</button>
-            <button className="adm-register-button">Cadastrar</button>
+            <button className="adm-register-button" onClick={() => console.log(addedTeacher)}>Cadastrar</button>
         </div>
       </div>
     </section>
