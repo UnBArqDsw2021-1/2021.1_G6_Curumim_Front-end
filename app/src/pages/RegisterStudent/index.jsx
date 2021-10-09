7549ad7c7b57ddf374fb671830e143b0417d5b2f
enzoggqs <eggqsaraiva@gmail.com>
2021-10-09 17:12:23 -0300

    Implementando funcionalidades da tela de registrar aluno

--- app/src/pages/RegisterStudent/index.jsx
+++ app/src/pages/RegisterStudent/index.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 
 import './styles.css';
 
 function RegisterStudent() {
+  const [name, setName] = useState('')
+  const [cpf, setCPF] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [cep, setCEP] = useState('');
+
+  const addedStudent = {
+    student: name,
+    CPF: cpf,
+    email: email,
+    addres: address,
+    CEP: cep
+  }
+
   return(
     <section className="adm-register">
       <Header />
@@ -13,28 +27,28 @@ function RegisterStudent() {
         <form className="adm-register-form">
           <div className="input-register-student">
             <h4>Nome:</h4>
-            <input type="text" placeholder="Nome da criança"/>
+            <input onChange={(e) => setName(e.currentTarget.value)} type="text" placeholder="Nome da criança"/>
           </div>
           <div className="input-register-student">
             <h4>CPF do responsável:</h4>
-            <input type="number" placeholder="CPF do responsável"/>
+            <input onChange={(e) => setCPF(e.currentTarget.value)} type="number" placeholder="CPF do responsável"/>
           </div>
           <div className="input-register-student">
             <h4>E-mail dos responsável:</h4>
-            <input type="email" placeholder="E-mail do responsável" />
+            <input onChange={(e) => setEmail(e.currentTarget.value)} type="email" placeholder="E-mail do responsável" />
           </div>
           <div className="input-register-student">
             <h4>Endereço:</h4>
-            <input type="text" placeholder="Endereço da criança"/>
+            <input onChange={(e) => setAddress(e.currentTarget.value)} type="text" placeholder="Endereço da criança"/>
           </div>
           <div className="input-register-student">
             <h4>CEP:</h4>
-            <input type="number" placeholder="CEP da criança"/>
+            <input onChange={(e) => setCEP(e.currentTarget.value)} type="number" placeholder="CEP da criança"/>
           </div>
         </form>
         <div className="adm-register-buttons">
             <button className="adm-cancel-register">Cancelar</button>
-            <button className="adm-register-button">Cadastrar</button>
+            <button className="adm-register-button" onClick={() => console.log(addedStudent)} >Cadastrar</button>
         </div>
       </div>
     </section>
