8f57e8b877792ead69bc19401da6923c11d4c3ae
enzoggqs <eggqsaraiva@gmail.com>
2021-10-09 17:17:59 -0300

    Adicionando funcionalidades da tela de registrar evento

--- app/src/pages/RegisterEvent/index.jsx
+++ app/src/pages/RegisterEvent/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 
 import './styles.css';
 
 function RegisterEvent() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [data, setData] = useState('');
+  const [local, setLocal] = useState('');
+
+  const addedEvent = {
+    name: name,
+    description: description,
+    data: data,
+    local: local
+  }
+
   return(
     <section className="adm-register">
       <Header />
@@ -13,24 +25,24 @@ function RegisterEvent() {
         <form className="adm-register-form">
           <div className="input-register-event">
               <h4>Nome:</h4>
-              <input type="text" placeholder="Nome do evento"/>
+              <input type="text" placeholder="Nome do evento" onChange={(e) => setName(e.currentTarget.value)}/>
           </div>
           <div className="input-register-event">
               <h4>Descrição:</h4>
-              <input type="text" placeholder="Nome do evento"/>
+              <input type="text" placeholder="Nome do evento" onChange={(e) => setDescription(e.currentTarget.value)}/>
           </div>
           <div className="input-register-event">
               <h4>Data:</h4>
-              <input type="date" placeholder="Data do evento"/>
+              <input type="date" placeholder="Data do evento" onChange={(e) => setData(e.currentTarget.value)}/>
           </div>
           <div className="input-register-event">
               <h4>Local:</h4>
-              <input type="text" placeholder="Local do evento"/>
+              <input type="text" placeholder="Local do evento" onChange={(e) => setLocal(e.currentTarget.value)}/>
           </div>
         </form>
         <div className="adm-register-buttons">
             <button className="adm-cancel-register">Cancelar</button>
-            <button className="adm-register-button">Cadastrar</button>
+            <button className="adm-register-button" onClick={() => console.log(addedEvent)}>Cadastrar</button>
         </div>
       </div>
     </section>
