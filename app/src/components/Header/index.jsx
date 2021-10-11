1a0b1c8a7be1c93c18facba0ddbcc51a17ed8265
enzoggqs <eggqsaraiva@gmail.com>
2021-10-11 02:36:53 -0300

    Criando funcionalidade do botão de voltar

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './styles.css';
+
 import Back from '../../assets/images/left-arrow-white.png'
+import { useHistory } from "react-router-dom";
 
 import {logout} from '../../utils/storage'
 
-class Header extends Component {
-    render() {
-        function activateLasers(event) {
-            logout()
-        }
-
-        return (
-            <div className="Header">
-                <img src={Back} alt="" />
-                <h2 onClick={activateLasers}>Curumim</h2>
-                <div className="sidebar">
-                </div>
-            </div>
-        );
+const Header = () => {
+    function activateLasers(event) {
+        logout()
     }
+
+    let history = useHistory();
+
+    return (
+        <div className="Header">
+            <img src={Back} onClick={() => history.goBack()} alt="voltar" />
+            <h2 onClick={activateLasers}>Curumim</h2>
+            <div className="sidebar">
+            </div>
+        </div>
+    );
+
 }
 
 export default Header;
